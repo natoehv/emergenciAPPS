b25ccc663e60a6d37f34ee0d35daff2c66ba11bf
Cristobal <Cristobal@Cristbal>
2014-10-08 19:26:49 -0300

    Se inicia el desarrollo de listar contactos

---
+++ emergenciAPPS/src/com/example/adapters/ListaAdapterContacto.java
@@ -0,0 +1,48 @@
+package com.example.adapters;
+
+import java.util.ArrayList;
+
+import com.example.emergenciapps.R;
+import com.example.object.Configuracion;
+import com.example.object.Contacto;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class ListaAdapterContacto extends ArrayAdapter{
+	private ArrayList<Contacto> objects;
+	private Context context;
+	
+	public ListaAdapterContacto(Context context, int textViewResourceId,ArrayList values) {
+		super(context, textViewResourceId, values);
+		
+		this.objects = values;
+		this.context = context;
+	}
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		View v = convertView;
+		final LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		v = inflater.inflate(R.layout.lista_contactos, null);
+		TextView nombre = (TextView) v.findViewById(R.id.nombre);
+		TextView numero = (TextView) v.findViewById(R.id.numero);
+		ImageView gps = (ImageView) v.findViewById(R.id.imagenGps);
+		ImageView email = (ImageView) v.findViewById(R.id.imagenEmail);
+		ImageView sms = (ImageView) v.findViewById(R.id.imagenSMS);
+		
+		Button edit = (Button) v.findViewById(R.id.btnEdit);
+		
+		nombre.setText(objects.get(position).getNombre());
+		numero.setText(objects.get(position).getNumero());
+		
+		return parent;
+		
+	}
+
+}
--- emergenciAPPS/src/com/example/contactos/ListaContactosActivity.java
+++ emergenciAPPS/src/com/example/contactos/ListaContactosActivity.java
@@ -1,14 +1,33 @@
 package com.example.contactos;
 
+import java.util.ArrayList;
+
+import com.example.adapters.ListaAdapterContacto;
+import com.example.emergenciapps.R;
+import com.example.object.Contacto;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class ListaContactosActivity extends Activity{
-
+	private ListView listaContactos;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		
 		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_contactos);
+		listaContactos = (ListView) findViewById(R.id.listaNroContactos);
+	
+		ArrayList listaContact = new ArrayList<Contacto>();
+		Contacto c = new Contacto();
+		c.setNombre("Juan");
+		c.setNumero("86542353");
+		listaContact.add(c);
+		
+		ListaAdapterContacto lista = new ListaAdapterContacto(this, R.id.lista_boton_llamar, listaContact);
+		listaContactos.setAdapter(lista);
 		
 		
 	}
