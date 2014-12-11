ba59be548897a31ed29ac5c35ff7cb1046170c87
Cristobal <Cristobal@Cristbal>
2014-12-11 13:01:31 -0300

    se agrega vibracion y sonido a notificacion

--- emergenciAPPS/src/com/example/login/GCMIntentService.java
+++ emergenciAPPS/src/com/example/login/GCMIntentService.java
@@ -1,15 +1,19 @@
 package com.example.login;
 
 import com.example.emergenciapps.EmergenciAPPSActivity;
+import com.example.emergenciapps.R;
 import com.example.seguimiento.SeguimientoActivity;
 import com.google.android.gms.gcm.GoogleCloudMessaging;
 
 import android.app.IntentService;
+import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
@@ -58,7 +62,7 @@ public class GCMIntentService extends IntentService
             		break;
             	case NOTIF_SEGUIMIENTO:
             		Log.d("NOTIFICACION_SEGUIMIENTO","NOTIFICACION EN PROCESO");
-            		this.mostrarNotification(extras.getString("msg"), "El usuario "+extras.getString("nombre")+ " necesita ayuda!");
+            		this.mostrarNotification(extras.getString("msg"), extras.getString("nombre")+ " necesita ayuda!");
             		break;
             	}
             }
@@ -69,12 +73,19 @@ public class GCMIntentService extends IntentService
 	
 	private void mostrarNotification(String msg, String title){
 		NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);	
+		long[] pattern = new long[]{2000, 200, 2000};
+		Uri defaultSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+		
 		NotificationCompat.Builder mBuilder = 
 			new NotificationCompat.Builder(this)  
-				.setSmallIcon(android.R.drawable.stat_sys_warning)  
+				.setSmallIcon(R.drawable.ic_launcher)  
 				.setContentTitle(title)
 				.setContentText(msg)
-				.setTicker(title);
+				.setTicker(title)
+				.setVibrate(pattern)
+				.setAutoCancel(true)
+				.setPriority(Notification.PRIORITY_MAX)
+				.setSound(defaultSound);
 		
 		Intent notIntent =  new Intent(this, SeguimientoActivity.class);
 		PendingIntent contIntent = PendingIntent.getActivity(this, 0, notIntent, 0);   
